665725f213d4fedea397209cd231761f216290f2
Luiz Guilherme <luizg.cozer@gmail.com>
2019-02-11 21:30:19 -0200

    Inicio da inclusão das operações

--- src/br/estacio/programacaoii/ui/calculadora.java
+++ src/br/estacio/programacaoii/ui/calculadora.java
@@ -5,6 +5,8 @@
  */
 package br.estacio.programacaoii.ui;
 
+import java.util.List;
+import java.util.ArrayList;
 import static javax.management.Query.value;
 
 /**
@@ -12,13 +14,14 @@ import static javax.management.Query.value;
  * @author aluno
  */
 public class calculadora extends javax.swing.JFrame {
+
     private int resultado;
-    
+
     public calculadora() {
         initComponents();
         this.resultado = 0;
     }
-    
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -87,12 +90,27 @@ public class calculadora extends javax.swing.JFrame {
         barra.setFont(new java.awt.Font("Calibri Light", 1, 14)); // NOI18N
         barra.setText("/");
         barra.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        barra.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                barraActionPerformed(evt);
+            }
+        });
 
         sete.setFont(new java.awt.Font("Calibri Light", 1, 14)); // NOI18N
         sete.setText("7");
+        sete.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                seteActionPerformed(evt);
+            }
+        });
 
         huit.setFont(new java.awt.Font("Calibri Light", 1, 14)); // NOI18N
         huit.setText("8");
+        huit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                huitActionPerformed(evt);
+            }
+        });
 
         nuit.setFont(new java.awt.Font("Calibri Light", 1, 14)); // NOI18N
         nuit.setText("9");
@@ -105,33 +123,78 @@ public class calculadora extends javax.swing.JFrame {
         vezes.setFont(new java.awt.Font("Calibri Light", 1, 14)); // NOI18N
         vezes.setText("X");
         vezes.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        vezes.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                vezesActionPerformed(evt);
+            }
+        });
 
         quatre.setFont(new java.awt.Font("Calibri Light", 1, 14)); // NOI18N
         quatre.setText("4");
+        quatre.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                quatreActionPerformed(evt);
+            }
+        });
 
         cinc.setFont(new java.awt.Font("Calibri Light", 1, 14)); // NOI18N
         cinc.setText("5");
+        cinc.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cincActionPerformed(evt);
+            }
+        });
 
         six.setFont(new java.awt.Font("Calibri Light", 1, 14)); // NOI18N
         six.setText("6");
+        six.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                sixActionPerformed(evt);
+            }
+        });
 
         menos.setFont(new java.awt.Font("Calibri Light", 1, 14)); // NOI18N
         menos.setText("-");
         menos.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        menos.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                menosActionPerformed(evt);
+            }
+        });
 
         jButton13.setFont(new java.awt.Font("Calibri Light", 1, 14)); // NOI18N
         jButton13.setText("0");
+        jButton13.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton13ActionPerformed(evt);
+            }
+        });
 
         jButton14.setFont(new java.awt.Font("Calibri Light", 1, 14)); // NOI18N
         jButton14.setText(",");
+        jButton14.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton14ActionPerformed(evt);
+            }
+        });
 
         igual.setFont(new java.awt.Font("Calibri Light", 1, 14)); // NOI18N
         igual.setText("=");
         igual.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        igual.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                igualActionPerformed(evt);
+            }
+        });
 
         mais.setFont(new java.awt.Font("Calibri Light", 1, 14)); // NOI18N
         mais.setText("+");
         mais.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        mais.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                maisActionPerformed(evt);
+            }
+        });
 
         un.setFont(new java.awt.Font("Calibri Light", 1, 14)); // NOI18N
         un.setText("1");
@@ -143,9 +206,19 @@ public class calculadora extends javax.swing.JFrame {
 
         trois.setFont(new java.awt.Font("Calibri Light", 1, 14)); // NOI18N
         trois.setText("3");
+        trois.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                troisActionPerformed(evt);
+            }
+        });
 
         duix.setFont(new java.awt.Font("Calibri Light", 1, 14)); // NOI18N
         duix.setText("2");
+        duix.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                duixActionPerformed(evt);
+            }
+        });
 
         input_operacao.setBorder(javax.swing.BorderFactory.createEtchedBorder());
         input_operacao.addActionListener(new java.awt.event.ActionListener() {
@@ -209,9 +282,9 @@ public class calculadora extends javax.swing.JFrame {
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(21, 21, 21)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(input_operacao, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -275,21 +348,23 @@ public class calculadora extends javax.swing.JFrame {
 
     private void acActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_acActionPerformed
         // TODO add your handling code here:
+        this.input_operacao.setText("");
     }//GEN-LAST:event_acActionPerformed
 
     private void unActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_unActionPerformed
         this.addValues("1");
     }//GEN-LAST:event_unActionPerformed
 
-    private void addValues(String value){
-      String btn = this.input_operacao.getText();
-      input_operacao+=value;
-      this.input_operacao.setText(value);
+    private void addValues(String value) {
+        String btn = this.input_operacao.getText();
+        btn += value;
+        this.input_operacao.setText(btn);
     }
-    
-    
+
+
     private void nuitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_nuitActionPerformed
         // TODO add your handling code here:
+        this.addValues("9");
     }//GEN-LAST:event_nuitActionPerformed
 
     private void offActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_offActionPerformed
@@ -300,8 +375,101 @@ public class calculadora extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_input_operacaoActionPerformed
 
+    private void duixActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_duixActionPerformed
+        // TODO add your handling code here:
+        this.addValues("2");
+    }//GEN-LAST:event_duixActionPerformed
+
+    private void jButton13ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton13ActionPerformed
+        // TODO add your handling code here:
+        this.addValues("0");
+    }//GEN-LAST:event_jButton13ActionPerformed
+
+    private void troisActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_troisActionPerformed
+        // TODO add your handling code here:
+        this.addValues("3");
+    }//GEN-LAST:event_troisActionPerformed
+
+    private void quatreActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_quatreActionPerformed
+        // TODO add your handling code here:
+        this.addValues("4");
+    }//GEN-LAST:event_quatreActionPerformed
+
+    private void cincActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cincActionPerformed
+        // TODO add your handling code here:
+        this.addValues("5");
+    }//GEN-LAST:event_cincActionPerformed
+
+    private void sixActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_sixActionPerformed
+        // TODO add your handling code here:
+        this.addValues("6");
+    }//GEN-LAST:event_sixActionPerformed
+
+    private void seteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_seteActionPerformed
+        // TODO add your handling code here:
+        this.addValues("7");
+    }//GEN-LAST:event_seteActionPerformed
+
+    private void huitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_huitActionPerformed
+        // TODO add your handling code here:
+        this.addValues("8");
+    }//GEN-LAST:event_huitActionPerformed
+
+    private void jButton14ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton14ActionPerformed
+        // TODO add your handling code here:
+        this.addValues(",");
+    }//GEN-LAST:event_jButton14ActionPerformed
+
+    private void barraActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_barraActionPerformed
+        // TODO add your handling code here:
+        this.addValues("/");
+    }//GEN-LAST:event_barraActionPerformed
+
+    private void vezesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_vezesActionPerformed
+        // TODO add your handling code here:
+        this.addValues("*");
+    }//GEN-LAST:event_vezesActionPerformed
+
+    private void menosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menosActionPerformed
+        // TODO add your handling code here:
+        this.addValues("-");
+    }//GEN-LAST:event_menosActionPerformed
+
+    private void maisActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_maisActionPerformed
+        // TODO add your handling code here:
+        this.addValues("+");
+    }//GEN-LAST:event_maisActionPerformed
+
+    private void igualActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_igualActionPerformed
+        // TODO add your handling code here:
+        System.out.println(this.input_operacao.getText());
+    }//GEN-LAST:event_igualActionPerformed
+
+    private void calculo() {
+        List<Double> valores = new ArrayList<>();
+        List<Character> operadores = new ArrayList<>();
+
+        String formula = this.input_operacao.getText();
+        
+        for(int i=0; i<formula.length(); i++){
+            if(this.isOperador(formula.charAt(i))){
+                operadores.add(formula.charAt(i));
+            }else{
+                valores.add(Double.parseDouble(String.valueOf(formula.charAt(i))));
+            }
+            System.out.println(formula.charAt(i));
+        }
+    }
+    
+    private boolean isOperador(char value){
+        if(value == '+' || value == '-' || value == '*' || value == '/'){
+            return true;
+        }
+        return false;
+    }
+
     public static void main(String args[]) {
-     
+
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new calculadora().setVisible(true);
